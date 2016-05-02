692f8ac220dff94b311e44199b97f93c326f4972
Anastasiya Bila <vypadkova@gmail.com>
2016-05-02 23:08:42 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//1
+function getNumbers(string) {
+  var array = [];
+  for (var i = 0; i < string.length ; i++){
+    var numb = parseInt(string[i]);
+      if(!isNaN(numb)) array.push(numb);
+    }
+    return array;
+}
+
+//2
+function getNumericSum(string) {
+  var all = 0;
+  for (var i = 0; i < string.length ; i++){
+    var numb = parseInt(string[i]);
+      if (!isNaN(numb)) all += numb;
+    }
+    return all;
+}
+
+//3 поки не працює,потім дороблю
+//4
+
+function getQuanOfUniqueCharacters(string) {
+    var all = 0;
+      string.split("").sort().forEach(function(item,i,array){
+        if(array[i] != array[i + 1]) all++;
+    });
+    return all;
+};
+//5
+
+function getPensionersName(string) {
+    var Pensioners_Name = [];
+    for (var i = 0; i < string.length; i++) {
+        if (string[i].age >= 65) {
+            Pensioners_Name = (string[i].name);
+        }
+    }
+    return Pensioners_Name;
+}
